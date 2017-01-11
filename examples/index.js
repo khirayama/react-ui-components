f8834b9a844c224a77f96ecfab4baa8d44a3e315
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-12 06:33:07 +0900

    Setup tab component

--- examples/index.js
+++ examples/index.js
@@ -4,8 +4,17 @@ import ReactDOM from 'react-dom';
 // list
 import ListContainer from './list/list-container';
 
+// tab
+import TabContainer from './tab/tab-container';
+
 window.addEventListener('DOMContentLoaded', () => {
+  // list
   if (document.querySelector('.list-component')) {
     ReactDOM.render(<ListContainer/>, document.querySelector('.list-component'));
+  }
+
+  // tab
+  if (document.querySelector('.tab-component')) {
+    ReactDOM.render(<TabContainer/>, document.querySelector('.tab-component'));
   }
 });
