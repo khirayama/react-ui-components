131004a17c6345c59a7801c4f129a0a1c0d129de
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-14 12:44:08 +0900

    Update index.js

---
+++ components/index.js
@@ -0,0 +1,29 @@
+import {
+  List,
+  ListItem,
+  ListItemContent,
+  ListItemLeftBackground,
+  ListItemRightBackground,
+} from './list';
+
+import {
+  Tab,
+  TabList,
+  TabListItem,
+  TabContentList,
+  TabContentListItem,
+} from './tab';
+
+export {
+  List,
+  ListItem,
+  ListItemContent,
+  ListItemLeftBackground,
+  ListItemRightBackground,
+
+  Tab,
+  TabList,
+  TabListItem,
+  TabContentList,
+  TabContentListItem,
+};
